6718204b798981827ef9fc498c0180619e353518
Deo <mpldeo@hotmail.com>
2015-12-13 19:37:31 +0100

    Basisversion

--- Watcher5_1_1/wear/src/main/java/com/example/watcher5_1_1/watcher5_1_1/station.java
+++ Watcher5_1_1/wear/src/main/java/com/example/watcher5_1_1/watcher5_1_1/station.java
@@ -2,6 +2,10 @@ package com.example.watcher5_1_1.watcher5_1_1;
 
 import android.os.Bundle;
 import android.app.Activity;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class station extends Activity {
 
@@ -10,5 +14,7 @@ public class station extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_station);
     }
+
+
 
 }
